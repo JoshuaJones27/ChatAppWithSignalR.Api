e96292cb88b1473624d3d5f00adc580e0f84fcdf
JoshuaJones27 <100417295+JoshuaJones27@users.noreply.github.com>
2023-02-19 11:29:53 +0000

    Initial commit

---
+++ ChatAppWithSignalR.Api/Entitites/ChatAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChatAppWithSignalR.Api.Entitites
+{
+    public class ChatAppContext : DbContext
+    {
+        public ChatAppContext(DbContextOptions<ChatAppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
---
+++ ChatAppWithSignalR.Api/Entitites/TblUser.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatAppWithSignalR.Api.Entitites
+{
+    public class TblUser
+    {
+        public int Id { get; set; }
+        public string LoginId { get; set; } = null;
+        public string UserName { get; set; } = null;
+        public string Password { get; set; } = null;
+        public byte[] StoreSalt { get; set; } = null;
+        public bool IsOnline { get; set; }
+        public DateTime LastLogonTime { get; set; }
+    }
+}
